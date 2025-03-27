7dbb810de0f2f59b97fd45879ffcbb4cc7832c0c
Sen B. Donissaint <donissaint13@gmail.com>
2025-03-27 11:05:26 -0400

    Started Menus & GameManager
    - Made Scenes (MainMenu, Level1)
    - Made Panels and Scripts for MainMenu and PauseMenu

---
+++ Tower of Madness/Assets/Script/PauseMenuController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuPanel;
+    private bool isPaused = false;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(isPaused){
+                ResumeGame();
+            }
+            else{
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame(){
+        //Show Pause Menu UI
+        pauseMenuPanel.SetActive(true);
+        //Freeze game time
+        Time.timeScale = 0f;
+        //(Optional) Freeze audio
+        //Audiolistener.pause = true;
+        isPaused = true;
+    }
+
+    public void ResumeGame(){
+        //Show Pause Menu UI
+        pauseMenuPanel.SetActive(false);
+        //Unfreeze game time
+        Time.timeScale = 1f;
+        //(Optional) Unfreeze audio
+        //Audiolistener.pause = false;
+        isPaused = false;
+    }
+
+    public void RestartLevel(){
+        if(GameManager.Instance != null){
+            GameManager.Instance.RestartLevel();
+        } else{
+            Debug.Log("Game Manager doesn't exist");
+        }
+    }
+
+    public void QuitGame(){
+        // If you're in the editor, this won't fully work,
+        // but in a built application, this will quit the game.
+        Application.Quit();
+        // If you have a Main Menu scene, you might do:
+        // SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Tower of Madness/Assets/Script/PlayerHealth.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+    // public Slider healthbar;
+    
+    // void Start()
+    // {
+    //     currentHealth = maxHealth;
+    //     healthbar.maxValue = maxHealth;
+    //     healthbar.value = currentHealth;
+    // }
+
+    // public void TakeDamage(int damage){
+    //     currentHealth -= damage;
+    //     healthbar.value = currentHealth;
+    //     if(currentHealth <= 0){
+    //         Die();
+    //     }
+    // }
+
+    // public void Die(){
+    //     if(GameManager.Instance != null){
+    //         GameManager.Instance.RestartGame();
+    //     }
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
