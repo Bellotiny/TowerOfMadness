f8eda87d90bb422ed3c074925e5b60f162e1e8c7
jannalmb00 <129775487+jannalmb00@users.noreply.github.com>
2025-04-29 11:23:21 -0400

    added platform shaker script

--- Assets/Script/PlatformShaker.cs
+++ Assets/Script/PlatformShaker.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlatformShaker : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float shakeDuration = 0.5f;
+    public float shakeAmount = 0.1f;
+    public float disappearDelay = 0.5f;
+
+    private Vector3 originalPosition;
+    private bool isActivated = false;
+
     void Start()
     {
-        
+        originalPosition = transform.localPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (!isActivated && other.CompareTag("Player"))
+        {
+            isActivated = true;
+            StartCoroutine(ShakeAndDisappear());
+        }
+    }
+
+    IEnumerator ShakeAndDisappear()
+    {
+        float elapsed = 0f;
+
+        while (elapsed < shakeDuration)
+        {
+            Vector3 randomOffset = Random.insideUnitSphere * shakeAmount;
+            transform.localPosition = originalPosition + new Vector3(randomOffset.x, randomOffset.y, 0f);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = originalPosition;
+        yield return new WaitForSeconds(disappearDelay);
+
+        gameObject.SetActive(false); // Or use Destroy(gameObject);
     }
 }
