cdf65465f8df542a4cfe7bfabd46dc60f3fee9e4
jannalmb00 <129775487+jannalmb00@users.noreply.github.com>
2025-04-29 21:54:52 -0400

    minor push

--- Assets/Script/SearchLightController.cs
+++ Assets/Script/SearchLightController.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class SearchLightController : MonoBehaviour
 {
-     public float angleRange = 45f;      // How far the light turns left and right
-    public float speed = 1f;            // Speed of the movement
+     public float angleRange = 45f;  // How far the light turns on each axis
+    public float speed = 1f;        // Speed of the movement
 
-    private float startAngle;
+    public bool rotateX = false;    // Toggle rotation on X axis
+    public bool rotateY = true;     // Toggle rotation on Y axis
+    public bool rotateZ = false;    // Toggle rotation on Z axis
+
+    private Vector3 startAngles;
 
     void Start()
     {
-        startAngle = transform.eulerAngles.y;
+        startAngles = transform.eulerAngles;
     }
 
     void Update()
     {
-        float angle = startAngle + Mathf.Sin(Time.time * speed) * angleRange;
-        transform.rotation = Quaternion.Euler(0, angle, 0);
+        float oscillation = Mathf.Sin(Time.time * speed) * angleRange;
+
+        float x = rotateX ? startAngles.x + oscillation : startAngles.x;
+        float y = rotateY ? startAngles.y + oscillation : startAngles.y;
+        float z = rotateZ ? startAngles.z + oscillation : startAngles.z;
+
+        transform.rotation = Quaternion.Euler(x, y, z);
     }
 }
