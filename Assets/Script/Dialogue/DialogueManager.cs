9fe0bd5acb9cb50825d17c98b97701ae37df6474
Bellotiny <donissaint13@gmail.com>
2025-05-22 03:12:35 -0400

    Spider movement & cutscene

---
+++ Assets/Script/Dialogue/DialogueManager.cs
@@ -0,0 +1,60 @@
+using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI speakerNameText;
+    public TextMeshProUGUI dialogueText;
+
+    public List<DialogueLine> lines = new List<DialogueLine>();
+    private int currentLineIndex = 0;
+    private bool isDialogueActive = false;
+
+    void Update()
+    {
+        if (isDialogueActive && Input.GetMouseButtonDown(0)) // Left click
+        {
+            ShowNextLine();
+        }
+    }
+
+    public void StartDialogue(List<DialogueLine> newLines)
+    {
+        Time.timeScale = 0f;
+        lines = newLines;
+        currentLineIndex = 0;
+        isDialogueActive = true;
+        dialoguePanel.SetActive(true);
+        ShowCurrentLine();
+    }
+
+    private void ShowCurrentLine()
+    {
+        DialogueLine line = lines[currentLineIndex];
+        speakerNameText.text = line.speaker;
+        dialogueText.text = line.text;
+    }
+
+    private void ShowNextLine()
+    {
+        currentLineIndex++;
+        if (currentLineIndex < lines.Count)
+        {
+            ShowCurrentLine();
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    private void EndDialogue()
+    {
+        dialoguePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isDialogueActive = false;
+    }
+}
