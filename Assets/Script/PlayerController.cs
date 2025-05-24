73c319b99b75ab556e8727a0ce5af760e220075e
samuelgallo1 <samuelcano0903@gmail.com>
2025-05-23 21:15:37 -0400

    to finish

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -10,13 +10,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float fallThresholdY = -10f;
     private PlayerHealth health;
     public Collider[] attackColliders;
+    private AudioSource audioSource;
+    public AudioClip footstepSound;
     void Start()
     {
         animator = GetComponent<Animator>();
         movement = GetComponent<CharacterMovement>();
         rb = GetComponent<Rigidbody>();
         health = GetComponent<PlayerHealth>();
-        foreach( Collider attackCollider in attackColliders)
+        audioSource = GetComponent<AudioSource>();
+        foreach (Collider attackCollider in attackColliders)
         {
             attackCollider.enabled = false; // Disable collider at start
         }
@@ -28,16 +31,26 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("CharacterSpeed", rb.velocity.magnitude);
         animator.SetBool("IsGrounded", movement.IsGrounded);
 
-        if(Input.GetButtonUp("Fire1")){
+        if (Input.GetButtonUp("Fire1"))
+        {
             //animator.SetTrigger("doRoll");
         }
 
-        if(transform.position.y < fallThresholdY){
+        if (transform.position.y < fallThresholdY)
+        {
             if (health != null)
             {
                 Debug.Log("Player has fallen!!!!");
                 health.Die();
             }
+        }
+        if (movement.IsGrounded && rb.velocity.magnitude > 0.1f)
+        {
+            if (!audioSource.isPlaying)
+                {
+                    Debug.Log("walking sound");
+                    audioSource.PlayOneShot(footstepSound);
+                }
         }
     }
     public void EnableHitbox()
