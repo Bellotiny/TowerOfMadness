fd6fa82e2c00dedd39c23879cd449f085c9c3a75
Bellotiny <donissaint13@gmail.com>
2025-05-23 20:23:13 -0400

    Cut scene mechanism

--- Assets/Script/Dialogue/DialogueManager.cs
+++ Assets/Script/Dialogue/DialogueManager.cs
@@ -2,13 +2,30 @@ using TMPro;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    [Header("Panel & Groups")]
     public GameObject dialoguePanel;
-    public TextMeshProUGUI speakerNameText;
-    public TextMeshProUGUI dialogueText;
-
+    public GameObject leftDialogueGroup;
+    public GameObject rightDialogueGroup;
+//------------------------------------------
+    [Header("Speakers")]
+    public string leftSpeakerName;
+    public string rightSpeakerName;
+//------------------------------------------
+    [Header("TextMesh Objects")]
+    public TextMeshProUGUI leftSpeakerNameText;
+    public TextMeshProUGUI leftDialogueText;
+    public Image leftSpeakerImage;
+    public TextMeshProUGUI rightSpeakerNameText;
+    public TextMeshProUGUI rightDialogueText;
+    public Image rightSpeakerImage;
+//------------------------------------------
+    [Header("Speaker Images")]
+    public Sprite leftSpeakerSprite;
+    public Sprite rightSpeakerSprite;
     public List<DialogueLine> lines = new List<DialogueLine>();
     private int currentLineIndex = 0;
     private bool isDialogueActive = false;
@@ -34,10 +51,35 @@ public class DialogueManager : MonoBehaviour
     private void ShowCurrentLine()
     {
         DialogueLine line = lines[currentLineIndex];
-        speakerNameText.text = line.speaker;
-        dialogueText.text = line.text;
-    }
 
+        if (line.speaker == rightSpeakerName)
+        {
+            leftDialogueGroup.SetActive(false);
+            rightDialogueGroup.SetActive(true);
+
+            rightSpeakerNameText.text = line.speaker;
+            rightDialogueText.text = line.text;
+            rightSpeakerImage.sprite = rightSpeakerSprite;
+        }
+        else if (line.speaker == leftSpeakerName)
+        {
+            rightDialogueGroup.SetActive(false);
+            leftDialogueGroup.SetActive(true);
+
+            leftSpeakerNameText.text = line.speaker;
+            leftDialogueText.text = line.text;
+            leftSpeakerImage.sprite = leftSpeakerSprite;
+        }
+        else
+        {
+            Debug.LogWarning($"Unknown speaker \"{line.speaker}\". Defaulting to left.");
+            rightDialogueGroup.SetActive(false);
+            leftDialogueGroup.SetActive(true);
+
+            leftSpeakerNameText.text = line.speaker;
+            leftDialogueText.text = line.text;
+        }
+    }
     private void ShowNextLine()
     {
         currentLineIndex++;
